75b2862984f863d0366fd37008654089593ad768
Carlos Lopez <cluis.lopez@gmail.com>
2018-11-16 00:43:34 +0100

    Take care

--- src/com/clopez/homecontrol/ControlMonitor.java
+++ src/com/clopez/homecontrol/ControlMonitor.java
@@ -1,15 +1,18 @@
 package com.clopez.homecontrol;
 
+import com.clopez.raspi.Caldera;
+
 public class ControlMonitor {
 
 	public static void main(String[] args) {
 		
-		Globals globals = new Globals();
+		Globals globals = new Globals("GLOBALS");
 		variablesExternas v = new variablesExternas("Properties"); // Fichero con variables externas del programa (IP Adresses, etc.)
 		Registro reg = new Registro("LOG"); // Fichero historico
 		
 		while (true) {
 			/* Control starts here */
+			boolean estado = Caldera.Estado(v.get("CalderaIP"));
 			
 			/* Monitor starts here */
 			try {
