7088e404d6f6bc395fce677f1b1d7b5959f39009
Carlos Lopez <cluis.lopez@gmail.com>
2018-11-27 01:18:44 +0100

    Sorry for the late update

--- WebContent/index.js
+++ WebContent/index.js
@@ -2,6 +2,7 @@
  * 
  */
 
+$(document).ready(function() {
 	
 	/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 	function myFunction() {
@@ -12,4 +13,29 @@
 	    x.style.display = "block";
 	  }
 	}
+	
+	 function monitorRefresh(){
+		 $("#refrescando").css("display", "block");
+		 $.get("ServerTest", function(data, status) {
+				$("#estado").text(data.estado);
+				$("#currentTemp").text(data.currentTemp);
+				$("#currentHum").text(data.currentHum);
+				var modo = "";
+				switch (data.modeOp) {
+				case 0:
+					modo = "OFF";
+					break;
+				case 1:
+					modo = "MANUAL";
+					break;
+				case 2:
+					modo = "PROGRAMADO";
+					break;
+				}
+				$("#modeOp").text(modo);
+				$("#tempTarget").text(data.tempTarget);
+			});
+	 }
+	 
+ });
 	
