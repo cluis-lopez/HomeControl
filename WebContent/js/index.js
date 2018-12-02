99325d6bb917e7fd1ed376e5d58e4dc5d8cb4ed5
Carlos Lopez <cluis.lopez@gmail.com>
2018-12-02 18:51:47 +0100

    Javascript corrections

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,13 +1,3 @@
-	/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-	function myFunction() {
-	  var x = document.getElementById("myLinks");
-	  if (x.style.display === "block") {
-	    x.style.display = "none";
-	  } else {
-	    x.style.display = "block";
-	  }
-	}
-
 $(document).ready(function() {
 	
 	var mapaModes = {"OFF":0, "MANUAL":1, "PROGRAMA":2};
@@ -15,6 +5,14 @@ $(document).ready(function() {
 	for (i = 10; i < 30; i=i+0.5){
 		$("#tempTarget_select").append("<option>"+i+"</option>");
 	};
+	
+	$(".icon").click(function toggleMenu(){
+		if ($("#myLinks").css("display") == "block")
+			$("#myLinks").css("display", "none");
+		else
+			$("#myLinks").css("display", "block");
+			
+	});
 	
 	monitorRefresh();
 	 
@@ -36,7 +34,7 @@ $(document).ready(function() {
 		 $("#historic").css("display","none");
 		 $("#about").css("display","none");
 		 $("#modeOp_select").val($("#modeOp").text());
-		 if (isNaN$("#tempTarget").text().split(" ")[0]) // True if this is Not a Number
+		 if (isNaN($("#tempTarget").text().split(" ")[0])) // True if this is Not a Number
 			 $("#tempTarget_select").val("20");
 		 else
 			 $("#tempTarget_select").val($("#tempTarget").text().split(" ")[0]);
@@ -68,23 +66,29 @@ $(document).ready(function() {
 		 $("#about").css("display","block");
 	 });
 	 
-	 function monitorRefresh(){
+	 function monitorRefresh() {
 		 $("#refrescando").css("display", "block");
-		 $.get("ControlServlet", function(data, status) {
-				$("#estado").text(data.estado);
-				$("#currentTemp").text(data.currentTemp);
-				$("#currentHum").text(data.currentHum);
-				modo = Object.keys(mapaModes)[data.modeOp];
-				$("#modeOp").text(modo);
-				if (data.tempTarget == "9999")
-					data.tempTarget="N.A.";
-				$("#tempTarget").text(data.tempTarget+"  &degC");
-				$("#tempTarget2").text(data.tempTarget+"  &degC");
-				$("#refrescando").css("display", "none");
+		 $.ajax({ cache: false,
+			    url: "ControlServlet",
+			    success: function (data) {
+			    	$("#estado").text(data.estado);
+					$("#currentTemp").text(data.currentTemp);
+					$("#currentHum").text(data.currentHum);
+					modo = Object.keys(mapaModes)[data.modeOp];
+					$("#modeOp").text(modo);
+					if (data.tempTarget == "9999")
+						data.tempTarget="N.A.";
+					$("#tempTarget").text(data.tempTarget+"  &degC");
+					$("#tempTarget2").text(data.tempTarget+"  &degC");
+					$("#refrescando").css("display", "none");
+			    },
+			    error: function (xhr, ajaxOptions, thrownError) {
+			    	$("#refrescando").css("display", "none");
+			        alert("Ha habido un problema al comunicarse con el servidor " + thrownError);
+			    },
+			    timeout: 5000
 			});
-		 
 		 var chart = lastChart();
-		 //chart.resize();
 	 }
 	 
 	 
@@ -107,23 +111,35 @@ $(document).ready(function() {
 	};
 	
 	function postea(modeClient, tempClient){
-	 $("#refrescando").css("display", "block");
-	 $("#control_button").text("Activando")
-	 $.post("ControlServlet",
-			    {
-		 			clientMode: modeClient,
-		 			clientTemp:tempClient
-			    },
-			    function(data, status){
-			        if (data == "OK" && status == "success"){
-			        	$("#refrescando").css("display", "none");
-			   	 		$("#control_button").css("background-color","lightskyblue");
-			   	 		$("#control_button").text("Activar")
-			   	 		$("#control_button").prop("disabled", false);
-			        } else {
-			        	alert("Algo ha ido mal al conectarnos con el sistema");
-			        }
-			    });
+		$("#refrescando").css("display", "block");
+		$("#control_button").text("Activando");
+		$.ajax({
+			url: "ControlServlet", 
+			data: {clientMode: modeClient,
+				clientTemp:tempClient},
+				type: 'post',
+				error: function(XMLHttpRequest, textStatus, errorThrown){
+					$("#refrescando").css("display", "none");
+					$("#control_button").css("background-color","lightskyblue");
+					$("#control_button").text("Activar")
+					$("#control_button").prop("disabled", false);
+					alert('Problemas de xonexion. Status:' + XMLHttpRequest.status);
+				},
+				success: function(data){
+					if (data == "OK"){
+						$("#refrescando").css("display", "none");
+						$("#control_button").css("background-color","lightskyblue");
+						$("#control_button").text("Activar")
+						$("#control_button").prop("disabled", false);
+					} else {
+						$("#refrescando").css("display", "none");
+						$("#control_button").css("background-color","lightskyblue");
+						$("#control_button").text("Activar")
+						$("#control_button").prop("disabled", false);
+						alert("Algo ha ido mal al conectarnos con el sistema");
+					}
+				}
+		});
 	}
 	 
  });
